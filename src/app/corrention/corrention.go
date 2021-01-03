4c28ebe3c12d1ebfa9a4f86dd474f61439e8f499
new5tt <chn_614si@163.com>
2021-01-04 03:05:28 +0800

    temp

--- src/app/corrention/corrention.go
+++ src/app/corrention/corrention.go
@@ -1,9 +1,11 @@
 package corrention
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"gin-vue-admin/pkg/response"
+	"io/ioutil"
 	"net/http"
 	"reflect"
 	"time"
@@ -21,10 +23,19 @@ type textCorrection interface {
 
 // provider 服务提供者
 type provider struct {
-	name    string
-	text    string
-	service string
+	name string
+	text string
 }
+
+type CorrentionParams struct {
+	Content []struct {
+		Text string `json:"text" binding:"required"`
+		Line string `json:"line" binding:"required"`
+	} `josn:"content"`
+	Total string `json:"total" binding:"required"`
+}
+
+var textArr CorrentionParams
 
 // corrention 文本纠错函数
 func (p *provider) corrention() string {
@@ -63,12 +74,12 @@ func Call(m map[string]interface{}, name string, params ...interface{}) (result 
 	result = f.Call(in)
 	return
 }
-func Handle(c *gin.Context) {
+func Handle(c *gin.Context, providerName string) {
 	funcs := map[string]interface{}{
 		"xfyun": xfyun,
 	}
 
-	if result, err := Call(funcs, "xfyun", c); err == nil {
+	if result, err := Call(funcs, providerName, c); err == nil {
 		fmt.Println("result:", result)
 		for _, r := range result {
 			fmt.Printf(" return: type=%v, value=[%d]\n", r.Type(), r.Int())
@@ -80,19 +91,29 @@ func Handle(c *gin.Context) {
 
 // NlpTextCorrention 入口
 func NlpTextCorrention(c *gin.Context) {
-	name := c.DefaultQuery("provider_name", "xfyun")
-	newProvider := &provider{
-		name:    name,
-		text:    "时代财经智能化",
-		service: "textcorrention",
+	// providerName := c.DefaultQuery("provider_name", "xfyun")
+	bodyData, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		response.FailResult(501, err.Error(), c)
 	}
+	fmt.Println(bodyData)
 
-	Handle(c)
+	err = json.Unmarshal(bodyData, &textArr)
+	if err != nil {
+		response.FailResult(500, err.Error(), c)
+	}
+	fmt.Println(textArr)
+	// newProvider := &provider{
+	// 	name: providerName,
+	// 	text: "dd",
+	// }
 
-	var newTextCorrection textCorrection
-	newTextCorrection = newProvider
-	response.SuccessResult(gin.H{
-		"text": newProvider.name,
-	}, c)
-	fmt.Println(newTextCorrection.corrention())
+	// Handle(c, providerName)
+
+	// var newTextCorrection textCorrection
+	// newTextCorrection = newProvider
+	// response.SuccessResult(gin.H{
+	// 	"text": newProvider.name,
+	// }, c)
+	// fmt.Println(newTextCorrection.corrention())
 }
--- src/internal/middleware/correntionparams.go
+++ src/internal/middleware/correntionparams.go
@@ -4,18 +4,19 @@ import (
 	"gin-vue-admin/pkg/response"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
 
 type CorrentionParams struct {
-	Content struct {
+	Content []struct {
 		Text string `json:"text" binding:"required"`
-		Line int    `json:"line" binding:"required,min=1"`
+		Line string `json:"line" binding:"required"`
 	} `josn:"content"`
-	Total int `json:"total" binding:"required,min=1"`
+	Total string `json:"total" binding:"required"`
 }
 
 type publicCorrentionParams struct {
-	Charset      string `form:"charset" json:"charset" binding:"required,min=1"`
+	Charset      string `form:"charset" json:"charset" binding:"required"`
 	ProviderName string `form:"provider_name" json:"provider_name" `
 	Signature    string `form:"signature" json:"signature"  binding:"required"`
 	Date         string `form:"date" format_date:"200601020000" json:"date" binding:"required"`
@@ -24,20 +25,21 @@ type publicCorrentionParams struct {
 func CheckCorrentionParams() gin.HandlerFunc {
 	var (
 		cp CorrentionParams
-		pp publicCorrentionParams
+		// pp publicCorrentionParams
 	)
 
 	return func(c *gin.Context) {
 
-		if err := c.ShouldBindQuery(&pp); err != nil {
-			response.FailResult(500, err.Error(), c)
-			c.Abort()
-		}
+		// if err := c.ShouldBindQuery(&pp); err != nil {
+		// 	response.FailResult(500, err.Error(), c)
+		// 	c.Abort()
+		// }
 
-		if err := c.ShouldBind(&cp); err != nil {
-			response.FailResult(500, err.Error(), c)
+		if err := c.ShouldBindBodyWith(&cp, binding.JSON); err != nil {
+			response.FailResult(502, err.Error(), c)
 			c.Abort()
 		}
+		// c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(&cp))
 		c.Next()
 	}
 }
